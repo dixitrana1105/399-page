c66f1bc64e6562a429d3c25963a249d17bc70d42
Dixit <Dixit@gmail.com>
2025-01-16 13:05:44 +0530

    make proper set

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>#1 Best Hair Salon in Dubai | 499AED Hair Package Bestseller Dubai | Top Rated In UAE</title>
+    <base href="/">
+    <link rel="canonical" href="https://www.499hairpackage.com/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+        content="Change Your Hair Look with Mirrors Beauty Lounge. We Provide Top Rated Hair Salon Services. Hair styles, hair color, hair treatments and more. Call &amp; BOok Today! Feels Amazing&nbsp;with&nbsp;new&nbsp;hairs.">
+    <meta name="keywords"
+        content="Best hair salon Dubai, hair salons near me, ladies hair salon in Dubai, women hair salon near me, hairdresser Dubai, best hair cutting salon in Dubai, Best salon for hair color in Dubai, best hair coloring salon for ladies, best hair colorist in Dubai, best hair color salon in Dubai, hair colorists, hair package dubai, hair color deals Dubai, best hair stylist&nbsp;in&nbsp;Dubai">
+    <meta name="google-site-verification" content="I86wB9nL99DEKHxotirriUNAQNFkpXuCdwCFcM5h35o">
+    <style>
+        :root {
+            --blue: #007bff;
+            --indigo: #6610f2;
+            --purple: #6f42c1;
+            --pink: #e83e8c;
+            --red: #dc3545;
+            --orange: #fd7e14;
+            --yellow: #ffc107;
+            --green: #28a745;
+            --teal: #20c997;
+            --cyan: #17a2b8;
+            --white: #fff;
+            --gray: #6c757d;
+            --gray-dark: #343a40;
+            --primary: #007bff;
+            --secondary: #6c757d;
+            --success: #28a745;
+            --info: #17a2b8;
+            --warning: #ffc107;
+            --danger: #dc3545;
+            --light: #f8f9fa;
+            --dark: #343a40;
+            --breakpoint-xs: 0;
+            --breakpoint-sm: 576px;
+            --breakpoint-md: 768px;
+            --breakpoint-lg: 992px;
+            --breakpoint-xl: 1200px;
+            --font-family-sans-serif: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+            --font-family-monospace: SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace
+        }
+
+        *,
+        ::after,
+        ::before {
+            box-sizing: border-box
+        }
+
+        html {
+            font-family: sans-serif;
+            line-height: 1.15;
+            -webkit-text-size-adjust: 100%;
+            -webkit-tap-highlight-color: transparent
+        }
+
+        body {
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 1.5;
+            color: #212529;
+            text-align: left;
+            background-color: #fff
+        }
+
+        @-webkit-keyframes progress-bar-stripes {
+            from {
+                background-position: 1rem 0
+            }
+
+            to {
+                background-position: 0 0
+            }
+        }
+
+        @-webkit-keyframes spinner-border {
+            to {
+                -webkit-transform: rotate(360deg);
+                transform: rotate(360deg)
+            }
+        }
+
+        @-webkit-keyframes spinner-grow {
+            0% {
+                -webkit-transform: scale(0);
+                transform: scale(0)
+            }
+
+            50% {
+                opacity: 1;
+                -webkit-transform: none;
+                transform: none
+            }
+        }
+
+        @media print {
+
+            *,
+            ::after,
+            ::before {
+                text-shadow: none !important;
+                box-shadow: none !important
+            }
+
+            @page {
+                size: a3
+            }
+
+            body {
+                min-width: 992px !important
+            }
+        }
+    </style>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="print" onload="this.media='all'"><noscript>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    </noscript>
+    <style>
+        @import url('https://fonts.cdnfonts.com/css/helios-antique');
+        @import url('https://fonts.cdnfonts.com/css/brittany-signature');
+        @import url('https://fonts.cdnfonts.com/css/archivo');
+        @import url('https://fonts.cdnfonts.com/css/adam-2');
+        @import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css");
+        @import url('https://fonts.cdnfonts.com/css/candlescript-demo-version');
+
+        html,
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Helios Antique', sans-serif;
+            font-size: 16px;
+            font-weight: 400;
+            scroll-behavior: smooth
+        }
+
+        @-webkit-keyframes mirroreffects {
+            from {
+                -webkit-transform: scale(1);
+                transform: scale(1);
+                -webkit-transform-origin: center center;
+                transform-origin: center center;
+                -webkit-animation-timing-function: ease-out;
+                animation-timing-function: ease-out
+            }
+
+            10% {
+                -webkit-transform: scale(.91);
+                transform: scale(.91);
+                -webkit-animation-timing-function: ease-in;
+                animation-timing-function: ease-in
+            }
+
+            17% {
+                -webkit-transform: scale(.98);
+                transform: scale(.98);
+                -webkit-animation-timing-function: ease-out;
+                animation-timing-function: ease-out
+            }
+
+            33% {
+                -webkit-transform: scale(.87);
+                transform: scale(.87);
+                -webkit-animation-timing-function: ease-in;
+                animation-timing-function: ease-in
+            }
+
+            45% {
+                -webkit-transform: scale(1);
+                transform: scale(1);
+                -webkit-animation-timing-function: ease-out;
+                animation-timing-function: ease-out
+            }
+        }
+    </style>
+    <link href="assets/css/style.css" rel="stylesheet" media="print" onload="this.media='all'"><noscript>
+        <link rel="stylesheet" href="assets/css/style.css">
+    </noscript>
+    <style>
+        @media (max-width:1080px) {}
+    </style>
+    <link href="assets/css/responsive.css" rel="stylesheet" media="print" onload="this.media='all'"><noscript>
+        <link rel="stylesheet" href="assets/css/responsive.css">
+    </noscript>
+    <!-- <link href="assets/css/swiper-bundle.min.css" rel="stylesheet"> -->
+    <!-- <link rel="icon" type="image/x-icon" href="favicon.ico"> -->
+    <link rel="shortcut icon" type="image/x-icon" href="https://ik.imagekit.io/mirrorskit/assets/img/favicon-16x16.png">
+
+    <style>
+        @font-face {
+            font-family: 'Brown Sugar', sans-serif;
+            src: url("https://ik.imagekit.io/mirrorskit/assets/499/img/aed-399-499/brown-sugar.ttf")
+        }
+    </style>
+
+
+    <style>
+        :root {
+            --swiper-theme-color: #007aff
+        }
+
+        :root {
+            --swiper-navigation-size: 44px
+        }
+    </style>
+    <link rel="stylesheet" href="assets/css/styles.a2f233a33eccd281.css" media="print" onload="this.media='all'"><noscript>
+        <link rel="stylesheet" href="assets/css/styles.a2f233a33eccd281.css">
+    </noscript>
+</head>
+
+<body>
+    <app-root></app-root>
+    <script src="assets/js/runtime.2162b3388ed4c174.js" type="module"></script>
+    <script src="assets/js/polyfills.49321badde250171.js" type="module"></script>
+    <script src="assets/js/main.57d2f05ec9fae90b.js" type="module"></script>
+
+
+    <script async="" src="https://www.googletagmanager.com/gtag/js?id=G-WQY6ED8QKJ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-WQY6ED8QKJ');
+    </script>&nbsp;
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/swiper-bundle.min.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.icon-tabler-menu-deep').click(function() {
+                $(".dv_navbar_mobile").addClass("display-block-sm");
+            });
+
+            $('.icon-tabler-x').click(function() {
+                $(".dv_navbar_mobile").removeClass("display-block-sm");
+            });
+
+            $('.dv_mobile_menu_a').click(function() {
+                $(".dv_navbar_mobile").removeClass("display-block-sm");
+            });
+        });
+    </script>
+
+</body><!-- Google tag (gtag.js) -->
+
+</html>
